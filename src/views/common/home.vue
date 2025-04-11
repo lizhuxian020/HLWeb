2917fff117aec5f6d5367e455b875437b9d8bb1f
lzx <lizhuxian@zzstc.cn>
2025-04-11 15:07:18 +0800

    4.10

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -2,6 +2,7 @@
 import {useStore} from "vuex";
 import {computed, onMounted, ref} from "vue";
 import MainNaviBar from "../layouts/main-navi-bar.vue";
+import {ArrowLeft, User, ArrowLeftBold, ArrowRightBold} from "@element-plus/icons-vue";
 
 // const store = useStore();
 // let token = computed(() => {
@@ -20,13 +21,48 @@ onMounted(() => {
   }, 1000)
 })
 
+
+let expand = ref(false);
+function clickArrow() {
+  console.log("click");
+  expand.value = !expand.value;
+}
 </script>
 
 <template>
   <main class="main" element-loading-text="加载中, 请稍后..." v-loading="loading" style="height: 100%">
-    <div v-if="!loading">
+    <template v-if="!loading">
       <main-navi-bar/>
-    </div>
+      <section style="height: 100%; background-color: chartreuse;display: flex">
+        <aside style="height: 100%; background-color: gold; display:flex;">
+          <nav style="background-color: #4191F1; height: 100%; width: 80px">
+            <div style="width: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; padding-top: 15px" @click="clickArrow">
+              <el-icon>
+                <ArrowLeftBold v-if="expand"/>
+                <ArrowRightBold v-else/>
+              </el-icon>
+            </div>
+            <div style="width: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; padding-top: 15px">
+              <el-icon><User/></el-icon>
+              <span>用户</span>
+            </div>
+            <div style="width: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; padding-top: 15px">
+              <el-icon><User/></el-icon>
+              <span>用户</span>
+            </div>
+            <div style="width: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; padding-top: 15px">
+              <el-icon><User/></el-icon>
+              <span>用户</span>
+            </div>
+          </nav>
+          <nav style="background-color: white; height: 100%; width: 180px">
+          </nav>
+        </aside>
+        <div style="height: 100%; background-color: darkgreen; flex: 1">
+
+        </div>
+      </section>
+    </template>
   </main>
 </template>
 
--- src/views/layouts/main-navi-bar.vue
+++ src/views/layouts/main-navi-bar.vue
@@ -5,7 +5,7 @@ import {House, User} from "@element-plus/icons-vue";
 </script>
 
 <template>
-  <div style="height: 50px; background-color: rebeccapurple; width: 100%">
+  <nav style="height: 50px; background-color: rebeccapurple; width: 100%">
     <div style="display: flex; height: 100%; align-items: center">
       <div style="background-color: darkgoldenrod; width: 80px; height: 100%; display: flex; align-items: center; justify-content: center;">
         <el-icon ><House /></el-icon>
@@ -20,14 +20,14 @@ import {House, User} from "@element-plus/icons-vue";
     </div>
 
 
-  </div>
+  </nav>
 </template>
 
 <style scoped lang="less">
 @import "@/assets/css/variable.less";
 
 :deep(.el-icon) {
-  color: @C2;
-  //color: red;
+  color: @C1;
+  font-size: 36px;
 }
 </style>
